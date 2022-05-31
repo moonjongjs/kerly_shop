22ee2f1953c5174d88c26ade463a07b158b2c3f6
moonjongjs <moonseonjong@naver.com>
2022-05-31 16:33:24 +0900

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import MemberComponent from './components/MemberComponent'
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <MemberComponent />
-);
